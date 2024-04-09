d29b358d5f37ad804ef058a8ca491190bc1224f4
Zivho <ziv.h252@gmail.com>
2024-04-09 19:27:35 +0200

    updating and fixing complexity

--- code/.ipynb_checkpoints/data_structures-checkpoint.py
+++ code/.ipynb_checkpoints/data_structures-checkpoint.py
@@ -26,49 +26,32 @@ class Edge:
 
 class Graph:
     def __init__(self):
-        self.vertices = []
+        self.vertices = {}
         self.edges = []
 
     def add_vertex(self, vertex):
-        self.vertices.append(vertex)
+        self.vertices[vertex.id] = vertex
 
     def add_edge(self, edge):
         self.edges.append(edge)
 
     def get_vertex(self, id):
-        for vertex in self.vertices:
-            if vertex.id == id:
-                return vertex
-        return None
-    def remove_edge(self, start, end):
-        for edge in self.edges:
-            if edge.start == start and edge.end == end:
-                self.edges.remove(edge)
-
-    def remove_vertex(self, id):
-        for vertex in self.vertices:
-            if vertex.id == id:
-                self.vertices.remove(vertex)
-
-    def update_x_y(self,id,x,y):
-      for vertex in self.vertices:
-            if vertex.id == id:
-              vertex.x = x
-              vertex.y = y
-
+        if id not in self.vertices:
+            return None
+        return self.vertices[id]
 
     def visualize_graph(self,custom_figsize,radius): # O(V + V + E) = O(2V + E) = O(V + E)
         self.place_vertices_in_circle(radius) # O(V)
         fig, ax = plt.subplots(figsize=(custom_figsize, custom_figsize))  # Adjust width and height as needed
         # Plot Vertices
         scaling_factor = 1 / len(self.vertices)
-        for vertex in self.vertices: # O(V)
+        for vertex in self.vertices.values(): # O(V)
             ax.scatter(vertex.x, vertex.y, color='blue', zorder=2, s= scaling_factor* 40000*(custom_figsize/15), edgecolors='black')
             ax.text(vertex.x, vertex.y, str(vertex.id), fontsize= (scaling_factor* 40000*(custom_figsize/15))/100, ha='center', va='center', zorder=len(self.vertices) * scaling_factor * 2, color='white')
         # Plot edges
         for edge in self.edges: #O(E)
-            start = edge.start
-            end = edge.end
+            start = self.vertices[edge.start.id]
+            end = self.vertices[edge.end.id]
             ax.plot([start.x, end.x], [start.y, end.y], color='gray', zorder=1, linewidth=0.5)
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
@@ -78,7 +61,7 @@ class Graph:
     def visualize_graph_Bonus(self,custom_figsize,radius): # O(V + V + E) = O(2V + E) = O(V + E)
         avg_nghb=0
         #compute average neighbors
-        for vertex in self.vertices:
+        for vertex in self.vertices.values():
           avg_nghb+= len(vertex.neighbors)
         avg_nghb= avg_nghb/len(self.vertices)
         print("avg neighbors",avg_nghb)
@@ -88,14 +71,14 @@ class Graph:
         # Plot Vertices
    # Plot Vertices
         scaling_factor = 1 / len(self.vertices)
-        for vertex in self.vertices: # O(V)
+        for vertex in self.vertices.values(): # O(V)
 
             ax.scatter(vertex.x, vertex.y, color='blue', zorder=2, s= scaling_factor* 40000*(custom_figsize/15), edgecolors='black')
             ax.text(vertex.x, vertex.y, str(vertex.id), fontsize= (scaling_factor* 40000*(custom_figsize/15))/100, ha='center', va='center', zorder=len(self.vertices) * scaling_factor * 2, color='white')
         # Plot edges
         for edge in self.edges: #O(E)
-            start = edge.start
-            end = edge.end
+            start = self.vertices[edge.start.id]
+            end = self.vertices[edge.end.id]
             ax.plot([start.x, end.x], [start.y, end.y], color='gray', zorder=1, linewidth=0.5)
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
@@ -112,7 +95,7 @@ class Graph:
         angle_increment = 2 * np.pi / num_vertices
 
         # Place vertices in a bit of randomized circle
-        for i, vertex in enumerate(self.vertices): # O(V)
+        for i, vertex in enumerate(self.vertices.values()): # O(V)
             angle = i * angle_increment
             vertex.x = center_x + radius * np.cos(angle)
             vertex.y = center_y + radius * np.sin(angle)
@@ -126,7 +109,7 @@ class Graph:
         angle_increment = 2 * np.pi / num_vertices
 
         # Place vertices in a bit of randomized circle
-        for i, vertex in enumerate(self.vertices): # O(V)
+        for i, vertex in enumerate(self.vertices.values()): # O(V)
             radius = given_radius
             if len(vertex.neighbors) > avg_nghb/3 and  len(vertex.neighbors) <= avg_nghb :
                 radius=radius-0.2*radius
@@ -228,6 +211,21 @@ class FVertex(Vertex):
 ################## classes parts 4,6 & 7  #####################
 
 class FSGraph(Graph):
+    def remove_edge(self, start, end):
+        for edge in self.edges:
+            if edge.start == start and edge.end == end:
+                self.edges.remove(edge)
+
+    def remove_vertex(self, id):
+        for vertex in self.vertices:
+            if vertex.id == id:
+                self.vertices.remove(vertex)
+
+    def update_x_y(self,id,x,y):
+      for vertex in self.vertices:
+            if vertex.id == id:
+              vertex.x = x
+              vertex.y = y
     def visualize_graph(self, custom_figsize, radius):  # O(V + V + E) = O(2V + E) = O(V + E)
 
       self.place_vertices_in_circle(radius)  # O(V)
